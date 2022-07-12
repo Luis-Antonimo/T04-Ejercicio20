e2aa4a1d518545408b87a0f30094c243e00a82c8
Luis Antónimo <luisantonimo@gmail.com>
2022-07-12 09:08:46 +0200

    ok

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace T04Ejercicio20
+{
+    class Program
+    {
+        static void Main()
+        {
+            int sumVentas = 0;
+            Console.WriteLine("Díga el número de ventas: ");
+            int numVentas = Convert.ToInt32(Console.ReadLine());
+            for (int x=1;x<=numVentas;x++)
+            {
+                Console.WriteLine("Indique el valor de la venta " + x + " :");
+                int valorVenta = Convert.ToInt32(Console.ReadLine());
+                sumVentas = sumVentas + valorVenta;
+            }
+            Console.WriteLine("La suma de todas las ventas es: " + sumVentas);
+        }
+    }
+}
 
